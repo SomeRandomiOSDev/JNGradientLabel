75763bfb918907c0d64cfd6a57bd5b14defcb872
Joe Newton <>
2019-06-11 08:02:11 -0400

    Update project structure. Added Carthage support. Added Travis-CI support. Added Swift-linting

--- JNGradientLabel.podspec
+++ JNGradientLabel.podspec
@@ -1,15 +1,7 @@
-#
-#  Be sure to run `pod spec lint JNGradientLabel.podspec' to ensure this is a
-#  valid spec and to remove all comments including this before submitting the spec.
-#
-#  To learn more about Podspec attributes see http://docs.cocoapods.org/specification.html
-#  To see working Podspecs in the CocoaPods repo see https://github.com/CocoaPods/Specs/
-#
-
 Pod::Spec.new do |s|
   
   s.name         = "JNGradientLabel"
-  s.version      = "1.0.0"
+  s.version      = "1.0.1"
   s.summary      = "A `UILabel` subclass with a gradient color"
   s.description  = <<-DESC
                    An iOS Swift framework that that provides a `UILabel` subclass that renders its text with a gradient color
@@ -22,9 +14,9 @@ Pod::Spec.new do |s|
   s.platform     = :ios, '8.0'
   s.source       = { :git => "https://github.com/SomeRandomiOSDev/JNGradientLabel.git", :tag => s.version.to_s }
   
-  s.source_files  = 'JNGradientLabel/Classes/**/*'
+  s.source_files  = 'JNGradientLabel/**/*.swift'
   s.frameworks    = 'UIKit', 'CoreGraphics'
-  s.swift_version = '4.0'
+  s.swift_version = '5.0'
   s.requires_arc  = true
   
 end
---
+++ JNGradientLabelExample/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  JNGradientLabel
+//
+//  Created by Joseph Newton on 01/20/2019.
+//  Copyright (c) 2019 Joseph Newton. All rights reserved.
+//
+
+import JNGradientLabel
+import UIKit
+
+// MARK: - Class (ViewController) Definition
+
+class ViewController: UIViewController {
+
+    // MARK: Interface Builder Outlets
+
+    @IBOutlet private weak var foregroundAxialGradientLabel: JNGradientLabel!
+    @IBOutlet private weak var backgroundAxialGradientLabel: JNGradientLabel!
+
+    @IBOutlet private weak var foregroundRadialGradientLabel: JNGradientLabel!
+    @IBOutlet private weak var backgroundRadialGradientLabel: JNGradientLabel!
+
+    // MARK: View Lifecycle Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let gradientColors: [UIColor]    = [.purple, .cyan]
+        let gradientlocations: [CGFloat] = [1.0, 0.0]
+
+        foregroundAxialGradientLabel.textGradientLocation = .foreground
+        foregroundAxialGradientLabel.setAxialGradientParameters(startPoint: CGPoint(x: 0.0, y: 0.0),
+                                                                endPoint: CGPoint(x: 1.0, y: 0.0),
+                                                                colors: gradientColors,
+                                                                locations: gradientlocations)
+
+        backgroundAxialGradientLabel.textColor = .white
+        backgroundAxialGradientLabel.textGradientLocation = .background
+        backgroundAxialGradientLabel.setAxialGradientParameters(startPoint: CGPoint(x: 0.0, y: 0.0),
+                                                                endPoint: CGPoint(x: 1.0, y: 0.0),
+                                                                colors: gradientColors,
+                                                                locations: gradientlocations)
+
+        foregroundRadialGradientLabel.textGradientLocation = .foreground
+        foregroundRadialGradientLabel.setRadialGradientParameters(startCenter: CGPoint(x: 0.5, y: 0.5),
+                                                                  startRadius: 0.0,
+                                                                  endCenter: CGPoint(x: 0.5, y: 0.5),
+                                                                  endRadius: 0.5,
+                                                                  colors: gradientColors,
+                                                                  locations: gradientlocations,
+                                                                  radiiScalingRule: .maximumBound)
+
+        backgroundRadialGradientLabel.textColor = .white
+        backgroundRadialGradientLabel.textGradientLocation = .background
+        backgroundRadialGradientLabel.setRadialGradientParameters(startCenter: CGPoint(x: 0.5, y: 0.5),
+                                                                  startRadius: 0.0,
+                                                                  endCenter: CGPoint(x: 0.5, y: 0.5),
+                                                                  endRadius: 0.5,
+                                                                  colors: gradientColors,
+                                                                  locations: gradientlocations,
+                                                                  radiiScalingRule: .maximumBound)
+    }
+}
